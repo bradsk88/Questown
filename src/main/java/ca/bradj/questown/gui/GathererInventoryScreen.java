4a1bdf1a9ad0c3cf6960865cb92dbce01ad13be5
Brad Johnson <bradsk88@gmail.com>
2023-07-01 13:05:02 -0600

    Gatherer: Render slots in UI
    Next:
    - Convert gatherer inventory to slots (force no stacking)
    - Create tag for "food"
    - Read/Write entity state to NBT

--- src/main/java/ca/bradj/questown/gui/GathererInventoryScreen.java
+++ src/main/java/ca/bradj/questown/gui/GathererInventoryScreen.java
@@ -2,12 +2,14 @@ package ca.bradj.questown.gui;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import mezz.jei.Internal;
+import mezz.jei.api.gui.drawable.IDrawableStatic;
 import mezz.jei.gui.elements.DrawableNineSliceTexture;
 import mezz.jei.gui.textures.Textures;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -18,11 +20,13 @@ public class GathererInventoryScreen extends AbstractContainerScreen<GathererInv
     private static final int backgroundHeight = 166;
 
     private final DrawableNineSliceTexture background;
+    private final IDrawableStatic slot;
 
     public GathererInventoryScreen(GathererInventoryContainer gathererInv, Inventory playerInv, Component title) {
         super(gathererInv, playerInv, title);
         Textures textures = Internal.getTextures();
         this.background = textures.getRecipeGuiBackground();
+        this.slot = textures.getSlotDrawable();
     }
 
     @Override
@@ -43,5 +47,8 @@ public class GathererInventoryScreen extends AbstractContainerScreen<GathererInv
         int x = (this.width - backgroundWidth) / 2;
         int y = (this.height - backgroundHeight) / 2;
         this.background.draw(stack, x, y, backgroundWidth, backgroundHeight);
+        for (Slot s : menu.slots) {
+            this.slot.draw(stack, x - 1 + s.x, y - 1 + s.y);
+        }
     }
 }
